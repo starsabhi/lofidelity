d37dd4a4a40e3b176656935a518cbf1139ec30c3
abhishek <starsabhishek@gmail.com>
2022-05-30 23:15:40 -0500

    Footer task is in progress

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-import LoginForm from './components/auth/LoginForm';
-import SignUpForm from './components/auth/SignUpForm';
+import LoginForm from "./components/auth/LoginForm";
+import SignUpForm from "./components/auth/SignUpForm";
 
-import NavBar from './components/NavBar';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/UsersList';
-import User from './components/User';
-import { authenticate } from './store/session';
+import NavBar from "./components/NavBar";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import UsersList from "./components/UsersList";
+import User from "./components/User";
+import { authenticate } from "./store/session";
+import Footer from "./components/Footer";
 
 function App() {
   const sessionUser = useSelector((state) => state.session.user);
@@ -40,15 +41,15 @@ function App() {
     <BrowserRouter>
       <NavBar />
       <Switch>
-        <Route exact path='/'>
+        <Route exact path="/">
           {sessionUser ? (
             // If logged in show landing pages
             sessionUser.isArtist ? (
               // id should be dynamically update based on artist domain name form Artist table
               //create artist store with userid hasmap
-              <Redirect to='/artist/testuser'></Redirect>
+              <Redirect to="/artist/testuser"></Redirect>
             ) : (
-              <Redirect to='/explore'></Redirect>
+              <Redirect to="/explore"></Redirect>
             )
           ) : (
             // else show splash page
@@ -56,23 +57,23 @@ function App() {
             <h1>WELCOME TO SPLASH!</h1>
           )}
         </Route>
-        <Route path='/explore'>
+        <Route path="/explore">
           {/* <ExplorePage isLoaded={isLoaded} /> */}
           {/* displays all the artists on the site */}
           <h1>WELCOME TO EXPLORE!</h1>
         </Route>
-        <Route path='/login' exact={true}>
+        <Route path="/login" exact={true}>
           <LoginForm />
         </Route>
-        <Route path='/sign-up' exact={true}>
+        <Route path="/sign-up" exact={true}>
           <SignUpForm />
         </Route>
-        <Route path='/:artistName' exact={true}>
+        <Route path="/:artistName" exact={true}>
           {/* redirect non-existent artist to signup form as query parameter, and update placeholder state based on query*/}
           <h1>WELCOME TO Artist Page!</h1>
         </Route>
         {/* move this route nested inside artist page */}
-        <Route path='/:artistName/album/:albumName' exact={true}></Route>
+        <Route path="/:artistName/album/:albumName" exact={true}></Route>
 
         <Route>
           <h1>Page Not Found </h1>
@@ -87,6 +88,7 @@ function App() {
           <h1>My Home Page</h1>
         </ProtectedRoute> */}
       </Switch>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ react-app/src/components/Footer/index.js
@@ -0,0 +1,82 @@
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footerDiv">
+      <div className="footDivContent">
+        <div className="developerList">
+          <div className="DevelopedbyClass">Developed by:</div>
+          <div className="smallName">
+            Anthony Lovern
+            <div className="divLinks">
+              <a href="https://github.com/Amlovern">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr3RdVtDNdUU-Zui7pAKxXoTeLUzT4TRsDYA&usqp=CAU"
+                  height="20px"
+                ></img>
+              </a>
+              <a href="https://github.com/Amlovern">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png"
+                  height="25px"
+                ></img>
+              </a>
+            </div>
+          </div>
+          <div className="smallName">
+            Elan Katz
+            <div className="divLinks">
+              <a href="https://github.com/otter23">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr3RdVtDNdUU-Zui7pAKxXoTeLUzT4TRsDYA&usqp=CAU"
+                  height="20px"
+                ></img>
+              </a>
+              <a href="https://www.linkedin.com/in/elankatz/">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png"
+                  height="25px"
+                ></img>
+              </a>
+            </div>
+          </div>
+          <div className="smallName">
+            Mark Osman
+            <div className="divLinks">
+              <a href="https://github.com/thisismydisplay">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr3RdVtDNdUU-Zui7pAKxXoTeLUzT4TRsDYA&usqp=CAU"
+                  height="20px"
+                ></img>
+              </a>
+              <a href="https://github.com/thisismydisplay">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png"
+                  height="25px"
+                ></img>
+              </a>
+            </div>
+          </div>
+          <div className="smallName">
+            Abhishek Bornak
+            <div className="divLinks">
+              <a href="https://github.com/starsabhi">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr3RdVtDNdUU-Zui7pAKxXoTeLUzT4TRsDYA&usqp=CAU"
+                  height="20px"
+                ></img>
+              </a>
+              <a href="https://www.linkedin.com/in/abhishek-bornak-semasna514865/">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png"
+                  height="25px"
+                ></img>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
