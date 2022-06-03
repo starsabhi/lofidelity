737637ff8191fdcca9059dcc24c8f922a2c74e6e
Anthony Lovern <voltorodarkwatch@gmail.com>
2022-06-03 13:00:46 -0400

    Splash page displaying correctly and favicon added

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,26 +1,20 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './SplashPage.css';
 
 const SplashPage = () => {
-  const dispatch = useDispatch();
   const artists = useSelector(state => state.artist.allArtists);
   const [index, setIndex] = useState(0);
   const [artistName, setArtistName] = useState("Lofi McLoferson");
   const [artistLocation, setArtistLocation] = useState('New York');
-  const [artistProfileImg, setArtistProfileImg] = useState('https://montessorimuddle.org/wp-content/uploads/2020/12/lofi_cali_girl_meme_by_yuumei_de4uk12-490.png')
-  const defaultArtist = {
-    name: "Lofi McLoferson",
-    location: "New York",
-    profileImageUrl: "https://montessorimuddle.org/wp-content/uploads/2020/12/lofi_cali_girl_meme_by_yuumei_de4uk12-490.png"
-  }
+  const [artistBackgroundImg, setArtistBackgroundImg] = useState('https://montessorimuddle.org/wp-content/uploads/2020/12/lofi_cali_girl_meme_by_yuumei_de4uk12-490.png')
 
   useEffect(() => {
     const interval = setTimeout(() => {
       setArtistName(artists[index].name)
       setArtistLocation(artists[index].location)
-      setArtistProfileImg(artists[index].profileImageUrl)
+      setArtistBackgroundImg(artists[index].bgImageUrl)
       if (index < artists.length - 1) {
         setIndex(index => index + 1)
       } else {
@@ -35,7 +29,7 @@ const SplashPage = () => {
   };
 
   return (
-    <div className='splash-page' style={{"backgroundImage": `url(${artistProfileImg})`}}>
+    <div className='splash-page' style={{"backgroundImage": `url(${artistBackgroundImg})`}}>
       <div className='splash-hooks'>
         <h1>Listen your way.</h1>
         <p>Whether listening to relax, focus on work, or anywhere in between...</p>
