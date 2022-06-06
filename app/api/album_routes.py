f7cc6c702cb766dc525cc9b8a29a14dba6369212
abhishek <starsabhishek@gmail.com>
2022-06-06 01:21:49 -0500

    Error handling fixed

--- app/api/album_routes.py
+++ app/api/album_routes.py
@@ -74,7 +74,7 @@ def create_new_album():
         image = request.files["image"]
 
         if not allowed_file(image.filename):
-            return {"errors": ["File type not permitted"]}, 400
+            return {"errors": ["File type not permitted (Only .png, .jpg, .jpeg, .gif permitted)"]}, 400
 
         image.filename = get_unique_filename(image.filename)
 
